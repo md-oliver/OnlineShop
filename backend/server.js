821ae74b081974a85fa70e69b26a8de5c7560054
mdoliver <mdoliver43@gmail.com>
2025-12-09 23:41:29 +0200

    Add colors and bcrypt packages. Add router and error handler routes to server

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from "express";
-import products from "./data/products.js";
+
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import productsRouter from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 connectDB();
@@ -13,18 +15,14 @@ app.get("/", (req, res, next) => {
     res.send("Hello World");
 });
 
-app.get("/api/products", (req, res, next) => {
-    res.json(products);
-});
-
-app.get("/api/products/:id", (req, res, next) => {
-    const product = products.find((prod) => prod._id === req.params.id);
-    res.json(product);
-});
+app.use("/api/products", productsRouter);
 
 // app.use((req, res, next) => {
 //     res.status(404).send("<h1>Page not found</h1>");
 // });
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log("Server listening on PORT: ", PORT);
